492466f392cf033a2a6d17c95f0c33926e5f88d3
Stephen <stephenlevesque2@gmail.com>
2023-05-04 22:20:39 -0700

    lab 14

---
+++ src/main/java/com/example/encryption/controller/EncryptionController.java
@@ -0,0 +1,17 @@
+package com.example.encryption.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class EncryptionController {
+    // to splash page
+    @GetMapping("/")
+    public String getHome(){
+        return login.html;
+    }
+// postmapping for signing up
+
+
+// post mapping for login
+}
